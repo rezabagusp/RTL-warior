4ca739d4802de0161f511dbba94b1db5c1002cf5
rezabagusp <rezabaguspermana.rbp@gmail.com>
2020-11-13 10:28:04 +0700

    feat: button component

---
+++ src/component/Button/index.tsx
@@ -0,0 +1,72 @@
+import React, {
+  FunctionComponent,
+  ReactNode,
+  MouseEvent,
+} from 'react';
+import cls from 'classnames';
+import './index.scss';
+
+export type Props = {
+  type?: 'primary' | 'secondary' | 'outline' | 'light-outline',
+  id?: string,
+  className?: string,
+  disabled?: boolean,
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void,
+  children: ReactNode,
+};
+
+const Button: FunctionComponent<Props> = (
+  {
+    type,
+    id,
+    className,
+    disabled,
+    onClick,
+    children,
+  },
+) => {
+  const baseClasses: { [key: string]: string } = {
+    btnPrimary: 'button--primary',
+    btnSecondary: 'button--secondary',
+    btnOutline: 'button--outline',
+  };
+
+  const propsClass: { [key: string]: boolean | undefined } = {
+    [baseClasses.btnPrimary]: type === 'primary',
+    [baseClasses.btnSecondary]: type === 'secondary',
+    [baseClasses.btnOutline]: type === 'outline',
+    [baseClasses.btnDisabled]: disabled,
+  };
+
+  const buttonClass = cls(
+    'button',
+    propsClass,
+    className,
+  );
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>): void => {
+    if (disabled) {
+      return;
+    }
+
+    onClick(e);
+  };
+
+  return (
+    <button
+      id={id}
+      className={buttonClass}
+      onClick={handleClick}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: 'primary',
+  disabled: false,
+};
+
+export default Button;
--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import React, {
+  FunctionComponent,
+} from 'react';
+import Button from '../component/Button';
+import './App.scss';
 
-function App() {
+const App: FunctionComponent<any> = () => {
+  const handleClickBtn = () => {
+    const text = 'hallo warior';
+    alert(text); // eslint-disable-line
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Button Component</h1>
+      <Button onClick={handleClickBtn}>
+        This is Button
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
