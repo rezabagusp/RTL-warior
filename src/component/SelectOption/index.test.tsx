1532a3ee338d580900642e4a594b541d65941731
rezabagusp <rezabaguspermana.rbp@gmail.com>
2020-11-16 11:01:03 +0700

    test: define possible test cases of SelectOption Component

---
+++ src/component/SelectOption/index.test.tsx
@@ -0,0 +1,21 @@
+import {
+  cleanup,
+} from '@testing-library/react';
+
+/*
+ * Test cases will consist of:
+ * 1. Should render into the DOM ✅
+ * 2. Click input bar should render dropdown list
+ * 3. On dropwdown open should listen arrow top & bottom
+ * 4. On select should hide dropdown
+ * 4.1 Select by click
+ * 4.2 Select by key enter
+ */
+
+afterEach(cleanup);
+
+describe.only('Select Option Component Test', () => {
+  test('1. Should render into DOM', () => {
+    expect(true).toBe(true);
+  });
+});
